b0669c152c64422a185bdc5d29b22127687feab5
SparxDani <86084707+SparxDani@users.noreply.github.com>
2024-05-27 17:24:02 -0500

    tarea

---
+++ Assets/script/birdScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class birdScript : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float _force;
+    [SerializeField] float ybound;
+
+    void Update()
+    {
+        
+    }
+
+
+}
